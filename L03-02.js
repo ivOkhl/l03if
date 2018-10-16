fa658b3cc5b680ed84c80cfe3fca040f756505b1
gavrilmikhailov <gavrilmikhailov@gmail.com>
2018-10-16 10:58:39 +0900

    first

--- L03-02.js
+++ L03-02.js
@@ -1,4 +1,14 @@
-// 2. Написать функцию f(a,b,c,d) = max(min(a,b), min(c,d))
 function f(a,b,c,d) {
+	var min1, min2, res;
 	
+	if (a<b) min1 = a;
+	else 	 min1 = b;
+	
+	if (c<d) min2 = c;
+	else 	 min2 = d;
+	
+	if (min1>min2) res = min1;
+	else		   res = min2;
+	
+	return res;
 }
