90a811b174bc2a960f5d977adf1301b551a8179b
unknown <ivan.okhl@gmail.com>
2018-10-17 03:12:17 +0900

    z3

---
+++ L03-12.js
@@ -0,0 +1,36 @@
+function digit(n) {
+	var n
+	switch(n){
+		case 1:
+		return 'ONE'
+		break
+		case 2:
+		return 'TWO'
+		break
+		case 3:
+		return 'THREE'
+		break
+		case 4:
+		return 'FOUR'
+		break
+		case 5:
+		return 'FIVE'
+		break
+		case 6:
+		return 'SIX'
+		break
+		case 7:
+		return 'SEVEN'
+		break
+		case 8:
+		return 'EIGHT'
+		break
+		case 9:
+		return 'NINE'
+		break
+		
+default:
+	return 'ERROR'
+
+	}
+}
