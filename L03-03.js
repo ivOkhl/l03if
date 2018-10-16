10574d85eb2b1c296d5223f9a83a3d6bc6ec7b2c
Cautics <maksim.andreev028291@gmail.com>
2018-10-16 11:06:57 +0900

    Update L03-03.js

--- L03-03.js
+++ L03-03.js
@@ -1,3 +1,18 @@
 function f(a,b,c,d) {
-	
+	var max1, max2, res;
+	if(Math.abs(a) < Math.abs(b))
+		min1 = Math.abs(a);
+	else
+		max1 - Math.abs(b);
+	if (Math.abs(c) > Math.abs(d))
+		max2 = ...;
+	else
+		...;
+
+	if (min1>max2)
+		res = min1;
+	else
+		res = max2;
+
+	return res;
 }
