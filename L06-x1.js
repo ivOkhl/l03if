90c76708da8b6a786ae8dfcc03dd008fdf097b65
slvkipriyanov <slvkipriyanov@gmail.com>
2018-10-16 09:10:24 +0900

    1 zadanie

---
+++ L06-x1.js
@@ -0,0 +1,19 @@
+function f(a, b, c, d) {
+	var m1, m2, res;
+	
+	if(a>b) {
+		m1 = a;
+	} else {
+		m1 = b;
+	}
+	if (c>d) {
+		m2 = c;
+	} else {
+		m2 = d;
+	}
+	if(m1<m2) {
+		res = m1;
+	} else {
+		res = m2;
+	}
+	return res;
