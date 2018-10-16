7faa117565a7646e0fc1e3a79509ec00055c5ec1
NikolaevEM <vassamax@mail.ru>
2018-10-16 11:26:37 +0900

    FirstHotuAS

---
+++ L03-11.js
@@ -0,0 +1,30 @@
+
+function WeakdayName(n){
+	switch(n){
+		case 1:{
+			return("понедельник");
+		}
+		case 2:{
+			return("вторник");
+		}
+		case 3:{
+			return("среда");
+		}
+		case 4:{
+			return("четверг");
+		}
+		case 5:{
+			return("пятница");
+		}
+		case 6:{
+			return("суббота");
+		}
+		case 7:{
+			return("делай реферат");
+		}
+		default:{
+			return("В неделе 7 дней");
+		}
+
+	}
+}
