f07e5438aeaa8a9c0015b6f63c650b5c27431903
Emelyanova <nomiama_ykt@mail.ru>
2018-10-16 09:04:18 +0900

    04

---
+++ L03-04.js
@@ -0,0 +1,16 @@
+// Написать функцию f(a,b,c,d) = max ( min (|a|,|b|), max (|c|,|d|)) 
+
+function f(a,b,c,d)
+{
+	var m1, m2, res;
+	
+	if (a>b) {
+		m1 = a;} 
+	else {
+		m1 = b; }
+	}
+	
+
+	
+	return res;
+}
