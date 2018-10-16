2c530df4cf930d5fd7e0a3d29237ae6884da8816
Slava Yaroev <vyacheslav.yaroev@mail.ru>
2018-10-16 11:11:56 +0900

    SL4

--- L03-04.js
+++ L03-04.js
@@ -1,1 +1,13 @@
-function f(a,b,c,d)
+function f(a,b,c,d) {
+	var max1,min1,res;
+	if (Math.abs(a) < Math.abs(b)) min1 = Math.abs(a);
+		else
+			min1 = Math.abs(b);
+	if (Math.abs(c) < Math.abs(d)) max1 = Math.abs(d);
+		else
+			max1 = Math.abs(c);
+	if (min1<max1) res = max1;
+		else
+			res = min1;
+	return res;
+}
