39ed14aa1191ed121b060f6e3bf84b4fa68a5090
VeniaminVV <vinichven@gmail.com>
2018-10-16 09:33:04 +0900

    ex1 ver2

--- L06-01.js
+++ L06-01.js
@@ -1,9 +1,10 @@
 function f(a,b,c,d){
 var min, max, result
-if (Math.abs(a)<Math.abs(b)) 
-{min = a;} else {min = b;}
-if (Math.abs(c)<Math.abs(d))
-{max=d;} else {max=c;}
+if (Math.abs(a)<=Math.abs(b)) 
+{min = Math.abs(a);} else {min = Math.abs(b);}
+if (Math.abs(c)<=Math.abs(d))
+{max=Math.abs(d);} else {max=Math.abs(c);}
 if (max>min)
 {result = max;} else {result = min;} 
-return result;}
+return result;
+}
