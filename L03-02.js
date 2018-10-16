ccc16cd6d8211e8d666afd605c48c995d6e9b330
gavrilmikhailov <gavrilmikhailov@gmail.com>
2018-10-16 11:26:03 +0900

    second

--- L03-02.js
+++ L03-02.js
@@ -1,14 +1,21 @@
 function f(a,b,c,d) {
+	
 	var min1, min2, res;
 	
-	if (a<b) min1 = a;
-	else 	 min1 = b;
+	if (a<b) 
+		min1 = a;
+	else
+		min1 = b;
 	
-	if (c<d) min2 = c;
-	else 	 min2 = d;
+	if (c<d)
+		min2 = c;
+	else
+		min2 = d;
 	
-	if (min1>min2) res = min1;
-	else		   res = min2;
+	if (min1>min2)
+		res = min1;
+	else
+		res = min2;
 	
 	return res;
 }
--- L03-07.js
+++ L03-07.js
@@ -1,3 +1,11 @@
-// 7. 	Напишите функцию S(x1,y1,x2,y2,x3,y3), которая принимает координаты трех точек на плоскости.
-// 		Если они могут быть вершинами равнобедренного треугольника, то функция дает его площадь,
-// 		в противном случае дает -1.
+function S(x1,y1,x2,y2,x3,y3) {
+	var b1,b2,b3,p,res;
+	b1 = Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+	b2 = Math.sqrt(Math.pow((x3-x2),2)+Math.pow((y3-y2),2));
+	b3 = Math.sqrt(Math.pow((x1-x3),2)+Math.pow((y1-y3),2));
+	p = (b1+b2+b3)/2;
+	if (b1==b2 || b2==b3 || b1==b3)
+		res = Math.sqrt(p*(p-b1)*(p-b2)*(p-b3));
+	else res = -1;
+	return res;
+}
