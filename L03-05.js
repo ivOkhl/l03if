b5bbd1a7886649e779f0d36e365ef7588d7e776a
Aina Gorokhova <ainomon1998q@gmail.com>
2018-10-16 09:26:00 +0900

    first

---
+++ L03-05.js
@@ -0,0 +1,15 @@
+function h(a,b,c)
+{
+	var s,p,res;
+	if (a+b>=c)
+	{
+		if (a+c>=b)
+		{
+			if (c+b>=a)
+			{
+				s=(a+b+c)/2;
+				res=(2*s)/a;
+			}else res=-1;
+		}else res=-1;
+	}else res=-1;
+}
--- L06-02.js
+++ L06-02.js
@@ -1,4 +1,29 @@
 function f(a,b,c,d)
 {
-	
+	var m1, m2,res;
+	if (a>b)
+	{
+		m1=b;
+	}
+	else
+	{
+		m1=a;
+	}
+	if (c>d)
+	{
+		m2=d;
+	}
+	else
+	{
+		m2=c;
+	}
+	if (m1>m2)
+	{
+		res=m1;
+	}
+	else
+	{
+		res=m2;
+	}
+	return(res);
 }
