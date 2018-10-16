d3c544f9a145bc11738acebcc2809814567638e5
kkq1l <glushkovu2000@gmail.com>
2018-10-16 11:19:31 +0900

    qweasdzxc

--- L03-04.js
+++ L03-04.js
@@ -1,3 +1,21 @@
 function f(a,b,c,d) {
+	var m1,m2,res;
+	if (Math.abs(a)>Math.abs(b)) {
+		m1 = b;
+	} else {
+		m1 = a;
+	}
 	
+	if (Math.abs(c)>Math.abs(d)) {
+		m2 = c;
+	} else {
+		m2 = d;
+	}
+	
+	if (m1>m2) {
+		res = m1; 
+	} else {
+		res = m2;
+	}
+	return res;
 }
