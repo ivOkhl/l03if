00de78320e0b04880ace1e69365a225dee7fdde7
skyyyx <thepro1999.vl@gmail.com>
2018-10-16 11:17:24 +0900

    2

--- L03-02.js
+++ L03-02.js
@@ -1,13 +1,13 @@
 function f(a,b,c,d){
-   var max1,max2,res;
-   if (Math.abs(a)>Math.abs(b))
+   var max1,max2,res,min1,min2;
+   if (Math.abs(a) < Math.abs(b))
       min1=Math.abs(a);
    else
-      min1=Math.abs(b);
-    if (Math.abs(c) > Math.abs(d))
-      max2= ...;
+      min2=Math.abs(b);
+    if (Math.abs(c) < Math.abs(d))
+      max1=Math.abs(c);
     else
-      ...;
+      max2=Math.abs(d);
 
     if (min1>max2)
       res = min1;
