9c445d38910cb4dee5409ea3e88f731da0636dad
Dyakonov Fedor <slvkipriyanov1@gmail.com>
2018-10-16 09:21:39 +0900

    z3

---
+++ L03-03.js
@@ -0,0 +1,18 @@
+function f(a,b,c,d) {
+var m1, m2, res;
+if (a>b) {
+	m1=a;
+}   else {
+	m1 = b;
+}
+ if c>d; {
+	 m2=d;
+ }  else {
+	 m2=d;
+ }
+ 
+ if (m1>m2) {
+	 res = m2;
+ }    else res = m1;
+return res;	
+}
