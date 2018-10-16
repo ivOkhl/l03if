e4f698fa431725b89eb3bcd84bd59289a8c6b7b2
unknown <asas200097@gmail.com>
2018-10-16 09:47:03 +0900

    наконец-то

--- L06-1.js
+++ L06-1.js
@@ -2,10 +2,8 @@ function Min(x,y,z){
 var min;
  if((x+y+z>=x*y*z)&&(x+y+z>=x*y+z))min=(x+y+z)
 	else{
-		if ((x*y*z>=x+y+z)&&(x*y*z>=x*y+z)) min=(x*y*z)	
-
+		if ((x*y*z>=x+y+z)&&(x*y*z>=x*y+z)) min=(x*y*z)
 			else {
-				if ((x*y+z>=x+y+z)&&(x*y+z>=x*y*z)) min=(x*y*z)}
-				return Min;
-}
+				if ((x*y+z>=x+y+z)&&(x*y+z>=x*y*z)) min=(x*y*z)}}
+				return min;
 }
