93becb50c630fa20547e0e694e774342df84d777
uufswe21 <sdfauren@gmail.com>
2018-10-16 10:23:06 +0900

    initial commit

---
+++ L03-03.js
@@ -0,0 +1,15 @@
+function f(a,b,c,d) { ///min(max(a,b),max(c,d))
+	
+}
+
+function min(a,b) {
+	if (a<b) return a;
+		else
+			return b;
+}
+
+function max(a,b) {
+	if (a>b) return a;
+		else
+			return b;	
+}
