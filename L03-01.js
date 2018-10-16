688edbea5d095e5d17d40660b0fd18d97cf232de
Aliev Bek <nomiama_ykt@mail.ru>
2018-10-16 09:27:12 +0900

    Hi

--- L03-01.js
+++ L03-01.js
@@ -1,3 +1,12 @@
+var min = 0;
 function f(x,y,z){
-	
+	if (x+y+z < x*y*z && x*y+z){
+		min = x + y + z;
+	}
+	if (x*y*z < x*y*z && x*y+z){
+		min = x*y*z;
+	}else{
+	    min = x*y+z;
+	}
+	return min;
 }
