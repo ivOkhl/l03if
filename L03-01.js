9e7834b859221d43340cbb161618ed9f76e9a927
aijysiana <aisen913@gmail.com>
2018-10-16 11:23:38 +0900

    01

--- L03-01.js
+++ L03-01.js
@@ -1,9 +1,9 @@
 function f(x,y,z){
 	var min;
-	if (x+y+z < x*y*z && x+y+z < x*y+z)
+	if (x+y+z <= x*y*z && x+y+z <= x*y+z)
 		min = x + y + z;
 	else
-		if (x*y*z < x+y+z && x*y*z < x*y+z)
+		if (x*y*z <= x+y+z && x*y*z <= x*y+z)
 			min = x*y*z;
 		else
 			min = x*y+z;
