cb841c2f766f198379676e4e2f9544f9fd25d270
ivanov innokenty <MidnightYKT@a.a>
2018-10-16 09:48:20 +0900

    z1

--- L03-02.js
+++ L03-02.js
@@ -1,2 +1,20 @@
 function f (a, b, c, d){
+	var m1, m2, res;
+	
+	if (a<b) {
+		m1 = a;
+	} else {
+        m1 = b;
+	}
+
+    if (c<d) {	
+	    m2 = c;
+	} esle {
+        m2 = d;
+     if (m1>m2){
+         res = m1;
+	 } else {
+         res = m2;
+	 }
+     return res;	 
 }
