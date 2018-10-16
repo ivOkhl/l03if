bcffd2311936ce5edd051ba70b318dd86504c70e
Glushkov Valery <fiit15@mail.ru>
2018-10-16 09:25:02 +0900

    01

--- L03-01.js
+++ L03-01.js
@@ -1,3 +1,15 @@
 function f(x,y,z) {
-	
+	var a,b,c,min;
+	a = x + y + z;
+	b = x * y * z;
+	c = x * y + z;
+	if (a < b && a < c) {
+		min = a;
+	} else
+	if (b < a && b < c) {
+		min = b;
+	} else {
+		min = c;
+	}
+	return min;
 }
